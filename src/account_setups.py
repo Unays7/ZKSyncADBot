44b1f7bab17381cbeb3469a9ac1a8f7642d4c59d
unays7 <unays.angus@gmail.com>
2023-10-20 14:03:49 +0100

    big fixes

--- src/account_setups.py
+++ src/account_setups.py
@@ -3,7 +3,7 @@ from eth_account import Account
 from utils.utils import *
 
 config = open_file("/Users/unaysangus/Desktop/Projects/Crypto Trading/DeFi/ZKSyncADBot/config.json")
-erc20_abi = open_file("/abi/ERC20.json")
+erc20_abi = open_file("/Users/unaysangus/Desktop/Projects/Crypto Trading/DeFi/ZKSyncADBot/abi/ERC20.json")
 eth_rpc = config["eth_rpc_url"]
 
 
@@ -51,28 +51,48 @@ def distribute_eth():
             print(f"Error")
 
 
+def getl2GasPrice(l1gas):
+    pubdataPriceETH = 17 * l1gas
+    minl2gasPriceEth = (pubdataPriceETH + 800 - 1) / 800
+    return max(minl2gasPriceEth, 500000000)
+
+
 def create_zk_transaction(wallet_address, private_key):
     w3 = Web3(Web3.HTTPProvider(eth_rpc))
     if not w3.is_connected():
         print("Not connected to Ethereum node.")
         exit(1)
-    zksync_abi = open_file("/abi/zksync.json")
-    contract_address = w3.to_checksum_address("0x32400084C286CF3E17e7B677ea9583e60a000324")
 
+    zksync_abi = open_file("/Users/unaysangus/Desktop/Projects/Crypto Trading/DeFi/ZKSyncADBot/abi/zksync.json")
+    contract_address = w3.to_checksum_address("0x32400084C286CF3E17e7B677ea9583e60a000324")
     zksync_contract = w3.eth.contract(address=contract_address, abi=zksync_abi)
-    amount = wei_to_ether(w3.eth.get_balance(wallet_address) - w3.eth.gas_price)
-    contract_call = zksync_contract.functions.requestL2Transaction(wallet_address, amount, b'', 21000, 80, [],
+
+    l1gas = w3.eth.gas_price
+
+    contract_call = zksync_contract.functions.requestL2Transaction(wallet_address, 1608960000000000200 + (
+                getl2GasPrice(l1gas) * 733664), b'', 733664, 800, [],
                                                                    wallet_address)
+
     transfer_txn = contract_call.build_transaction({
         'from': wallet_address,
-        'gas': 21000,
+        'gas': 80000,
         'gasPrice': w3.eth.gas_price,
         'nonce': w3.eth.get_transaction_count(wallet_address),
     })
 
-    signed_transaction = w3.eth.account.sign_transaction(transfer_txn, private_key)
-    txn = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-    print(f"***** Transaction: https://etherscan.io/tx/{txn} *****")
+    try:
+        signed_transaction = w3.eth.account.sign_transaction(transfer_txn, private_key)
+        txn = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+        print(f"Bridged for {wallet_address} with hash {txn.hex()}")
+        txn_receipt = w3.eth.wait_for_transaction_receipt(txn)
+        if txn_receipt['status']:
+            print(f"Bridge successful for {wallet_address}")
+            print(txn_receipt)
+        else:
+            print(f"Bridge failed for {wallet_address}")
+
+    except Exception as e:
+        print(f"Error")
 
 
 def bridge_to_zk():
@@ -82,8 +102,10 @@ def bridge_to_zk():
         print("Not connected to Ethereum node.")
         exit(1)
 
-    for pk in range(private_keys):
+    for pk in private_keys:
         account = Account.from_key(pk)
         wallet_address = w3.to_checksum_address(account.address)
-        create_zk_transaction(wallet_address, account)
+        create_zk_transaction(wallet_address, pk)
 
+
+bridge_to_zk()
